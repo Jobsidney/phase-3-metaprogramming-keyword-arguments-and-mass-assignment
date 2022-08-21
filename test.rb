67da48378b05295d8e01f5ff489cb4c4fcab971b
Jobsidney <jobsidney67@gmail.com>
2022-08-21 19:38:00 +0300

    Class mass assignment

---
+++ test.rb
@@ -0,0 +1,48 @@
+
+#Keyword Assignment to instance variables
+
+def print_name_and_greeting(greeting:, name:)
+    puts "#{greeting}, #{name}"
+  end
+
+  print_name_and_greeting(name:"Kanye", greeting:"Hi")
+  print_name_and_greeting(name:"Kanye",greeting: "Hi")
+
+
+
+
+  #MASS ASSIGNMENT
+  class Person
+    attr_accessor :name, :age
+  
+    def initialize(name:, age:)
+      @name = name
+      @age = age
+    end
+  end
+  person_aTT={name:"Shee",age:21}
+  shee=Person.new(**person_aTT)
+  p shee.age
+
+
+#MASS CLASS ASSIGNMENT 
+
+  class User
+    attr_accessor :name, :age, :location, :user_name
+  
+    def initialize(attributes)
+        attributes.each{
+            |key,value|
+            self.send("#{key}=", value)
+        }
+    end
+  end
+
+  twitter_user = { 
+    name: "Sophie", 
+    user_name: "sm_debenedetto", 
+    age: 26, 
+    location: "NY, NY"
+  }
+  twit=User.new(twitter_user)
+  puts twit.name
